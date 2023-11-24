2ddc0c8f02f7bc0d870d4157a5c35cf38da1385e
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-24 09:53:11 +0700

    Response Body

--- server.js
+++ server.js
@@ -15,17 +15,15 @@ const requestListener = (request, response) => {
   if (url === "/") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>");
+      response.end(JSON.stringify({message: `Ini adalah halaman utama`}));
     } else {
       response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
-      );
+      response.end(JSON.stringify({message: `Halaman tidak dapat diakses menggunakan ${method} request`}));
     }
   } else if (url === "/about") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+      response.end(JSON.stringify({message: "Halo! Ini adalah halaman about"}));
     } else if (method === "POST") {
       let body = [];
 
@@ -37,17 +35,17 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`Halo, ${name}! Ini adalah halaman about`);
+        response.end(JSON.stringify({message: `Halo, ${name}! Ini adalah halaman about`}));
       });
     } else {
       response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
-      );
+      response.end(JSON.stringify({message: `Halaman tidak dapat diakses menggunakan ${method} request`}));
     }
   } else {
     response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan!</h1>");
+    response.end(JSON.stringify({
+        message: "Halaman tidak ditemukan!"
+    }));
   }
 };
 
